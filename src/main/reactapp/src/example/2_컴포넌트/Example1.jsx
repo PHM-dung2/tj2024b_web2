539008e08bfc4448268d2ec09a9441af1debd294
PHM-dung2 <pp4992@naver.com>
2025-03-05 12:19:40 +0900

    day06 React 컴포넌트 3교시

--- src/main/reactapp/src/example/2_컴포넌트/Example1.jsx
+++ src/main/reactapp/src/example/2_컴포넌트/Example1.jsx
@@ -1,7 +1,7 @@
 // (1)
-export default function Example2( props ){
+export default function Example1( props ){
 
-    // ================ JSX 문법 s ================== // 
+    // ================== JSX 문법 s ================== // 
     return(
         <div>
             {/* 주석 내용물 */}
@@ -9,10 +9,15 @@ export default function Example2( props ){
             {/* 다른 컴포넌트 호출 */}
             <Component1 />
             <Component2 />
+            <Component3 />
+            <Component4 />
+            <Component5 />
+            <Component6 />
+            <Component7 />
         </div>
     )
     
-    // ================ JSX 문법 s ================== // 
+    // ================== JSX 문법 s ================== // 
 
 } // f end
 
@@ -39,5 +44,64 @@ function Component3( props ){
     )
 } // f end
 
+// (5)
+function Component4( props ){
+    const name = '유재석'
+    const age = 40;
+    const obj = { name, age }
+
+    return(
+        <>
+            <h4> { name } 님의 나이 : { age } </h4>
+            <h4> { obj.name } 님의 나이 : { obj.age } </h4>
+        </>
+    )
+} // f end
+
+// (6)
+function Component5( props ){
+    const loginState = true; // 로그인상태
+    // JSX 문법 { } 표현식에서는 if, for, function, const, let 불가능하다.
+    return (
+        <>
+            <h4> 로그인상태 : { loginState ? '로그인중' : '비로그인중' } </h4>
+            <h4> { loginState && <p> 로그인상태일때만 보이는 메시지 </p> } </h4>
+        </>
+    )
+} // f end
+
+// (7)
+function Component6( props ){
+    const items = [ '사과', '딸기', '바나나' ]
+    // JSX 문법의 목록( list, option 등등 ) 마크업들에는 key 속성 필요로 한다.
+        // => 리액트는 자동렌더링(새로고침) key 변화를 감지하여 변화된 key만 별도로 렌더링하는 기능
+    // JSX 문법에서는 forEach 함수보다 map 함수를 자주 사용된다.
+    return(<>
+        <ul>
+            { /* JSX 표현식 시작 */
+                items.map( 
+                    ( item, index ) => 
+                    ( <li key={index}> { item } </li> )
+                )/* map 시작 */
+            }
+        </ul>
+    </>)
+} // f end
+
+// (8)
+// * css 파일 import
+import './Example1.css' // * css 파일 import // import '경로/파일명.clsss'
+function Component7( props ){
+    // style 속성에 style="color:blue" [x]
+    // style 속성에 style={ {CSS객체} } [o]
+    // 주의할점 : css 카멜표기법, JS의 속성명은
+    const cssObj = { color : 'red', fontSize : 30 }
+    // class 속성이 아닌 className 사용한다.
+    return(<>
+        <h4 style={ { color : 'blue' } }> 인라인 형식의 CSS 넣기 </h4>
+        <h4 style={ cssObj } > 인라인 형식의 객체CSS 넣기 </h4>
+        <h4 className="myclass" > CSS파일 내 선택자 이용한 CSS 넣기 </h4>
+    </>)
+} // f end
 
 
--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -14,15 +14,19 @@ import App from './App.jsx'
 // (*) index.html에 root 구역 가져오기
 const root = createRoot( document.querySelector('#root') )
 
-// day1 : '컴포넌트 만들기' 렌더링
+// day01 : '컴포넌트 만들기' 렌더링
   // import 호출할컴포넌트명 form '경로/파일명(확장자);
 import Component1 from './example/1_동작구조/Component1.jsx'
 // root.render( <Component1 /> );
 
-// day1 : 여러 컴포넌트 만들기
+// day01 : 여러 컴포넌트 만들기
 import Component2 from './example/1_동작구조/Component2.jsx'
 // root.render( <Component2 /> );
 
-// day2 : 컴포넌트의 JSX 활용
+// day02 : 컴포넌트의 JSX 활용
 import Example1 from './example/2_컴포넌트/Example1.jsx'
-root.render( <Example1 /> )
+// root.render( <Example1 /> )
+
+// day02 : 컴포넌트의  props 활용
+import Example2 from './example/2_컴포넌트/Example2.jsx'
+root.render( <Example2 /> )
