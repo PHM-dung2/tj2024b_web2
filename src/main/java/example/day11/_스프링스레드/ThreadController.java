6a0a1078fa4da1ad618c375c55fb9ee6a000ad27
PHM-dung2 <pp4992@naver.com>
2025-03-25 13:15:42 +0900

    day11 스레드풀 예제 / 과제

---
+++ src/main/java/example/day11/_스프링스레드/ThreadController.java
@@ -0,0 +1,33 @@
+package example.day11._스프링스레드;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/day11/thread")
+@RequiredArgsConstructor
+public class ThreadController {
+
+    private final ThreadService threadService;
+
+    // (1) 비동기 = 기본값
+    @GetMapping("/start1")
+    public void thread1(){
+        System.out.println("ThreadController.thread1");
+        threadService.thread1();
+    }
+
+    // (2) 동기
+    @GetMapping("/start2")
+    public void thread2(){
+        System.out.println("ThreadController.thread2");
+        threadService.thread2();
+    }
+
+
+
+
+}
---
+++ src/main/java/example/day11/스프링스케줄링과제/AppStart.java
@@ -0,0 +1,15 @@
+package example.day11.스프링스케줄링과제;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run( AppStart.class );
+    }
+}
