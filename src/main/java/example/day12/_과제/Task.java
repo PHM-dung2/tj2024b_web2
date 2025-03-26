257b8579a646b7e4b96e5fe01a256cbe47332b92
PHM-dung2 <pp4992@naver.com>
2025-03-26 14:03:09 +0900

    day12 java 람다식 실습

--- src/main/java/example/day12/_과제/Task.java
+++ src/main/java/example/day12/_과제/Task.java
@@ -5,10 +5,19 @@ import lombok.NoArgsConstructor;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
 
 @NoArgsConstructor @Getter
-class Calculator{
+class MethodRef{
+    public List<String> 과일들;
+    public String 과일;
+    public void onForEach( List<String> 과일들 ){
+        과일들.stream().forEach( (과일) -> System.out.println( 과일 ) );
+    } // f end
 
+    public void onMessage( String 과일 ){
+        System.out.println( 과일 );
+    }
 }
 
 public class Task {
@@ -39,12 +48,28 @@ public class Task {
 
         // 5.[메소드 레퍼런스 사용함]  forEach함수를 활용하여 과일들을 하나씩 console에 출력하시오.
         System.out.println(">> 과제 5번 =================================================");
-
+        Supplier<MethodRef> ref = MethodRef::new;
+        MethodRef methodRef = ref.get();
+//        methodRef.onForEach( 과일들 );
+        과일들.forEach( methodRef::onMessage );
 
         // 6.[메소드 레퍼런스 사용함]  map함수를 활용하여 과일들을 하나씩 console에 출력하시오.
+        System.out.println(">> 과제 6번 =================================================");
+        과일들.stream()
+                .map( (과일) -> 과일 )
+                .forEach( methodRef::onMessage );
 
         // 7.[메소드 레퍼런스 사용함]  filter함수를 활용하여 과일들 중에 '바나나' 또는 '대추'만 console에 출력하시오.
+        System.out.println(">> 과제 7번 =================================================");
+        과일들.stream()
+                .filter( ( 과일 ) -> 과일 == "바나나" || 과일 == "대추" )
+                .forEach( methodRef::onMessage );
+
 
         // 8.[메소드 레퍼런스 사용함]  sorted함수를 활용하여 과일들을 내림차순으로 정렬하여 하나씩 console에 출력하시오.
+        System.out.println(">> 과제 8번 =================================================");
+        과일들.stream()
+                .sorted( Comparator.reverseOrder() )
+                .forEach( methodRef::onMessage );
     }
 }
